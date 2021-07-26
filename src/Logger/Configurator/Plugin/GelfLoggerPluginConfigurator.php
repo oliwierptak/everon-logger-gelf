1333ed0e49cfb177dd740ccdae5dd12120da1f31
Oliwier Ptak <oliwierptak@gmail.com>
2021-07-26 19:05:12 +0200

    upgraded to popo 5

--- src/Logger/Configurator/Plugin/GelfLoggerPluginConfigurator.php
+++ src/Logger/Configurator/Plugin/GelfLoggerPluginConfigurator.php
@@ -10,14 +10,10 @@ namespace Everon\Logger\Configurator\Plugin;
 
 use UnexpectedValueException;
 
-class GelfLoggerPluginConfigurator implements \Everon\Logger\Contract\Configurator\PluginConfiguratorInterface
+class GelfLoggerPluginConfigurator
 {
     protected const SHAPE_PROPERTIES = [
         'httpConfigurator' => \Everon\Logger\Configurator\Plugin\GelfHttpLoggerPluginConfigurator::class,
-        'logLevel' => 'null|string',
-        'pluginClass' => 'null|string',
-        'pluginFactoryClass' => 'null|string',
-        'shouldBubble' => 'null|bool',
         'tcpConfigurator' => \Everon\Logger\Configurator\Plugin\GelfTcpLoggerPluginConfigurator::class,
         'udpConfigurator' => \Everon\Logger\Configurator\Plugin\GelfUdpLoggerPluginConfigurator::class,
     ];
@@ -27,10 +23,6 @@ class GelfLoggerPluginConfigurator implements \Everon\Logger\Contract\Configurat
             'type' => 'popo',
             'default' => \Everon\Logger\Configurator\Plugin\GelfHttpLoggerPluginConfigurator::class,
         ],
-        'logLevel' => ['type' => 'string', 'default' => 'debug'],
-        'pluginClass' => ['type' => 'string', 'default' => null],
-        'pluginFactoryClass' => ['type' => 'string', 'default' => null],
-        'shouldBubble' => ['type' => 'bool', 'default' => true],
         'tcpConfigurator' => [
             'type' => 'popo',
             'default' => \Everon\Logger\Configurator\Plugin\GelfTcpLoggerPluginConfigurator::class,
@@ -43,16 +35,6 @@ class GelfLoggerPluginConfigurator implements \Everon\Logger\Contract\Configurat
 
     /** HTTP related options. */
     protected ?GelfHttpLoggerPluginConfigurator $httpConfigurator = null;
-
-    /** The minimum logging level at which this handler will be triggered */
-    protected ?string $logLevel = 'debug';
-    protected ?string $pluginClass = null;
-
-    /** Defines custom plugin factory to be used to create a plugin */
-    protected ?string $pluginFactoryClass = null;
-
-    /** Whether the messages that are handled can bubble up the stack or not */
-    protected ?bool $shouldBubble = true;
 
     /** TCP related options. */
     protected ?GelfTcpLoggerPluginConfigurator $tcpConfigurator = null;
@@ -96,145 +78,6 @@ class GelfLoggerPluginConfigurator implements \Everon\Logger\Contract\Configurat
     public function hasHttpConfigurator(): bool
     {
         return $this->httpConfigurator !== null || ($this->httpConfigurator !== null && array_key_exists('httpConfigurator', $this->updateMap));
-    }
-
-    /**
-     * The minimum logging level at which this handler will be triggered
-     */
-    public function setLogLevel(?string $logLevel): self
-    {
-        $this->logLevel = $logLevel; $this->updateMap['logLevel'] = true; return $this;
-    }
-
-    /**
-     * The minimum logging level at which this handler will be triggered
-     */
-    public function getLogLevel(): ?string
-    {
-        return $this->logLevel;
-    }
-
-    /**
-     * The minimum logging level at which this handler will be triggered
-     */
-    public function requireLogLevel(): string
-    {
-        if (static::METADATA['logLevel']['type'] === 'popo' && $this->logLevel === null) {
-            $popo = static::METADATA['logLevel']['default'];
-            $this->logLevel = new $popo;
-        }
-
-        if ($this->logLevel === null) {
-            throw new UnexpectedValueException('Required value of "logLevel" has not been set');
-        }
-        return $this->logLevel;
-    }
-
-    public function hasLogLevel(): bool
-    {
-        return $this->logLevel !== null || ($this->logLevel !== null && array_key_exists('logLevel', $this->updateMap));
-    }
-
-    public function setPluginClass(?string $pluginClass): self
-    {
-        $this->pluginClass = $pluginClass; $this->updateMap['pluginClass'] = true; return $this;
-    }
-
-    public function getPluginClass(): ?string
-    {
-        return $this->pluginClass;
-    }
-
-    public function requirePluginClass(): string
-    {
-        if (static::METADATA['pluginClass']['type'] === 'popo' && $this->pluginClass === null) {
-            $popo = static::METADATA['pluginClass']['default'];
-            $this->pluginClass = new $popo;
-        }
-
-        if ($this->pluginClass === null) {
-            throw new UnexpectedValueException('Required value of "pluginClass" has not been set');
-        }
-        return $this->pluginClass;
-    }
-
-    public function hasPluginClass(): bool
-    {
-        return $this->pluginClass !== null || ($this->pluginClass !== null && array_key_exists('pluginClass', $this->updateMap));
-    }
-
-    /**
-     * Defines custom plugin factory to be used to create a plugin
-     */
-    public function setPluginFactoryClass(?string $pluginFactoryClass): self
-    {
-        $this->pluginFactoryClass = $pluginFactoryClass; $this->updateMap['pluginFactoryClass'] = true; return $this;
-    }
-
-    /**
-     * Defines custom plugin factory to be used to create a plugin
-     */
-    public function getPluginFactoryClass(): ?string
-    {
-        return $this->pluginFactoryClass;
-    }
-
-    /**
-     * Defines custom plugin factory to be used to create a plugin
-     */
-    public function requirePluginFactoryClass(): string
-    {
-        if (static::METADATA['pluginFactoryClass']['type'] === 'popo' && $this->pluginFactoryClass === null) {
-            $popo = static::METADATA['pluginFactoryClass']['default'];
-            $this->pluginFactoryClass = new $popo;
-        }
-
-        if ($this->pluginFactoryClass === null) {
-            throw new UnexpectedValueException('Required value of "pluginFactoryClass" has not been set');
-        }
-        return $this->pluginFactoryClass;
-    }
-
-    public function hasPluginFactoryClass(): bool
-    {
-        return $this->pluginFactoryClass !== null || ($this->pluginFactoryClass !== null && array_key_exists('pluginFactoryClass', $this->updateMap));
-    }
-
-    /**
-     * Whether the messages that are handled can bubble up the stack or not
-     */
-    public function setShouldBubble(?bool $shouldBubble): self
-    {
-        $this->shouldBubble = $shouldBubble; $this->updateMap['shouldBubble'] = true; return $this;
-    }
-
-    /**
-     * Whether the messages that are handled can bubble up the stack or not
-     */
-    public function shouldBubble(): ?bool
-    {
-        return $this->shouldBubble;
-    }
-
-    /**
-     * Whether the messages that are handled can bubble up the stack or not
-     */
-    public function requireShouldBubble(): bool
-    {
-        if (static::METADATA['shouldBubble']['type'] === 'popo' && $this->shouldBubble === null) {
-            $popo = static::METADATA['shouldBubble']['default'];
-            $this->shouldBubble = new $popo;
-        }
-
-        if ($this->shouldBubble === null) {
-            throw new UnexpectedValueException('Required value of "shouldBubble" has not been set');
-        }
-        return $this->shouldBubble;
-    }
-
-    public function hasShouldBubble(): bool
-    {
-        return $this->shouldBubble !== null || ($this->shouldBubble !== null && array_key_exists('shouldBubble', $this->updateMap));
     }
 
     /**
@@ -316,10 +159,6 @@ class GelfLoggerPluginConfigurator implements \Everon\Logger\Contract\Configurat
     {
         $data = [
             'httpConfigurator' => $this->httpConfigurator,
-            'logLevel' => $this->logLevel,
-            'pluginClass' => $this->pluginClass,
-            'pluginFactoryClass' => $this->pluginFactoryClass,
-            'shouldBubble' => $this->shouldBubble,
             'tcpConfigurator' => $this->tcpConfigurator,
             'udpConfigurator' => $this->udpConfigurator,
         ];
@@ -374,30 +213,6 @@ class GelfLoggerPluginConfigurator implements \Everon\Logger\Contract\Configurat
         }
         catch (\Throwable $throwable) {
             $errors['httpConfigurator'] = $throwable->getMessage();
-        }
-        try {
-            $this->requireLogLevel();
-        }
-        catch (\Throwable $throwable) {
-            $errors['logLevel'] = $throwable->getMessage();
-        }
-        try {
-            $this->requirePluginClass();
-        }
-        catch (\Throwable $throwable) {
-            $errors['pluginClass'] = $throwable->getMessage();
-        }
-        try {
-            $this->requirePluginFactoryClass();
-        }
-        catch (\Throwable $throwable) {
-            $errors['pluginFactoryClass'] = $throwable->getMessage();
-        }
-        try {
-            $this->requireShouldBubble();
-        }
-        catch (\Throwable $throwable) {
-            $errors['shouldBubble'] = $throwable->getMessage();
         }
         try {
             $this->requireTcpConfigurator();
